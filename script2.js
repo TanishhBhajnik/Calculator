55cd3ce3157b36ddb1df0b8c99a7981dc6e3c3be
Tanishh <88125649+TanishhBhajnik@users.noreply.github.com>
2023-04-30 00:05:27 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,32 @@
+let string = "";
+let buttons = document.querySelectorAll('button');
+let operators = document.querySelectorAll('.operator');
+let input = document.querySelector('input');
+
+let array = Array.from(buttons);
+array.forEach(button => {
+    button.addEventListener('click', (e) => {
+        // console.log(e.target);
+
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.slice(0, string.length-1);
+            input.value = string;
+        }
+        
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+
+        }
+    });
+});
